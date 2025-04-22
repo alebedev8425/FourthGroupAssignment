46391729de6f93863f7831bf2966734c3fb773c0
Alexander Lebedev <alexanderlebedev@Alexanders-MacBook-Pro.local>
2025-04-22 10:25:28 -0500

    Adding the Grid class

--- src/hw4/maze/Grid.java
+++ src/hw4/maze/Grid.java
@@ -1,5 +1,33 @@
 package hw4.maze;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Grid {
+	
+	private List<Row> rows;
+	
+	/**
+     * @param creates a grid out of rows, must not be null rows
+     */
+    public Grid(List<Row> rows) {
+        this.rows = Objects.requireNonNull(rows);
+    }
+
+	public List<Row> getRows() {
+		return rows;
+	}
+
+	public void setRows(List<Row> rows) {
+		this.rows = rows;
+	}
+
+	@Override
+	public String toString() {
+		return "Grid [rows=" + rows + "]";
+	}
+    
+    
+	
 
 }
