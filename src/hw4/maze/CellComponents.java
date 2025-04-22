721c6349994533e3d89ffd3b32d5241574aa50e4
unknown <owenvenker@gmail.com>
2025-04-22 00:49:33 -0500

    implemented Row.java

--- src/hw4/maze/CellComponents.java
+++ src/hw4/maze/CellComponents.java
@@ -1,6 +1,39 @@
 package hw4.maze;
 
 public class CellComponents {
+private ComponentType type;
+    
+    public static final CellComponents WALL = new CellComponents(ComponentType.WALL);
+    public static final CellComponents APERTURE = new CellComponents(ComponentType.APERTURE);
+    public static final CellComponents EXIT = new CellComponents(ComponentType.EXIT);
+
+
+    public CellComponents(ComponentType type) {
+        this.type = type;
+    }
+
+    /*
+     * getter
+     */
+    public ComponentType getType() {
+        return type;
+    }
+
+    /*
+     * setter
+     */
+    public void setType(ComponentType type) {
+        this.type = type;
+    }
+    
+	/*
+	 * toString()
+	 */
+    @Override
+    public String toString() {
+        return type.toString();
+    }
 	
+
 
 }
